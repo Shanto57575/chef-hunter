0e744fec7ddf52213f883cac1a3671195bfd5278
Shanto57575 <shanto57575@gmail.com>
2023-05-04 22:35:14 +0600

    all done finally

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -2,12 +2,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCTpfRNuNgIulBXL3a2uxXBEoR2UZhCeqs",
-    authDomain: "chef-recipe-hunter-ec167.firebaseapp.com",
-    projectId: "chef-recipe-hunter-ec167",
-    storageBucket: "chef-recipe-hunter-ec167.appspot.com",
-    messagingSenderId: "405116401521",
-    appId: "1:405116401521:web:ca7fbfa9a6e39a218868d0"
+    apiKey: import.meta.env.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain,
+    projectId: import.meta.env.VITE_projectId,
+    storageBucket: import.meta.env.VITE_storageBucket,
+    messagingSenderId: import.meta.env.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId,
 };
 
 const app = initializeApp(firebaseConfig);
